947aa91da2623981161f8802cea61341ca74a82d
mabhishek7081 <mabhishek7081@gmail.com>
2022-12-17 19:09:09 +0530

    intial upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# viewcord-web
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import VideoPlayer from './components/VideoPlayer'
+
+const App = () =>  {
+ 
+ 
+  return (
+    <div className="App">
+      <VideoPlayer   />
+    </div>
+  )
+}
+
+export default App
