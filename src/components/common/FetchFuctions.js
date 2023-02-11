2749cd65c6702df72d424dbc00bdf3cdfaa9aa8c
mabhishek7081 <mabhishek7081@gmail.com>
2023-02-11 11:04:32 +0530

    added no-cors

--- src/components/common/FetchFuctions.js
+++ src/components/common/FetchFuctions.js
@@ -1,31 +1,31 @@
 export const getVideo = async (id) => {
     return  fetch(
-          `https://pipedapi.kavin.rocks/streams/${id}`
+          `https://pipedapi.kavin.rocks/streams/${id}`,{mode: 'no-cors'}
         ).then((response) => response.json());  
      
 } 
 
 export const getTrending = async (region) => {
     return  fetch(
-          `https://pipedapi.kavin.rocks/trending/?region=${region}`
+          `https://pipedapi.kavin.rocks/trending/?region=${region}`,{mode: 'no-cors'}
         ).then((response) => response.json());  
 }
 
 export const getChannel = async (channel_id) => {
   return fetch(
-    `https://pipedapi.kavin.rocks/channel/${channel_id}`
+    `https://pipedapi.kavin.rocks/channel/${channel_id}`, {mode: 'no-cors'}
   ).then((response) => response.json());
 
 }
 
 export const getSearchResults = async (query,filter) => {
   return fetch(
-    `https://pipedapi.kavin.rocks/search?q=${query}&filter=${filter}`
+    `https://pipedapi.kavin.rocks/search?q=${query}&filter=${filter}`, {mode: 'no-cors'}
   ).then((response) => response.json());
 }
 
 export const getPlaylist = async (playlist_id) => {
   return fetch(
-    `https://pipedapi.kavin.rocks/playlists/${playlist_id}`
+    `https://pipedapi.kavin.rocks/playlists/${playlist_id}`,{mode: 'no-cors'}
   ).then((response) => response.json());
 }
